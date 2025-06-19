51708011d45da83b108bc7aeb48faa93f552f446
kalandar <shaik.kalandar20@gmail.com>
2025-06-20 02:14:54 +0530

    chore: update vite.config.ts with correct base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/map-your-journey/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
